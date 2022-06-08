c626c45814788a47b9e35fec879617588e7e2c64
Colby Ford <colbytylerford@gmail.com>
2022-06-08 18:51:54 -0400

    Update network_generation.R

--- network_generation.R
+++ network_generation.R
@@ -1,6 +1,8 @@
-## Load in StrainHub
+## Load in Libraries
 library(strainhub)
-
+library(visNetwork)
+library(dplyr)
+library(randomcoloR)
 
 ### First Network
 ## Read in tree, metadata, and geodata
@@ -15,12 +17,31 @@ graph <- makeTransNet(treedata,
                       centralityMetric = 6,
                       treeType = "parsimonious")
 
-graph
+# graph
 
 saveRDS(graph, "graph1.rds")
+graph <- readRDS("graph1.rds")
 
 
+palette_20 <- distinctColorPalette(20)
 
+nodes <- graph$x$nodes %>%
+  mutate(shape = "dot",
+         color = palette_20,
+         font.size = 20)
+
+edges <- graph$x$edges %>%
+  mutate(arrows = "to",
+         smooth = TRUE,
+         color = ifelse(to == 5, "red", "grey"),
+         width = value,
+         value = NULL)
+
+## Generate the custom network using `visNetwork`
+visNetwork(nodes, edges) %>%
+  visOptions(nodesIdSelection = list(enabled = TRUE)) 
+
+####################################
 ### Second Network
 ## Read in tree, metadata, and geodata
 treedata <- ape::read.tree("RAxML_bestTree.COI_nataly_brenda_JANESSA.DEDUP.trim.tre")
@@ -36,3 +57,22 @@ graph <- makeTransNet(treedata,
 graph
 
 saveRDS(graph, "graph2.rds")
+graph <- readRDS("graph2.rds")
+
+palette_25 <- distinctColorPalette(25)
+
+nodes <- graph$x$nodes %>%
+  mutate(shape = "dot",
+         color = palette_25,
+         font.size = 20)
+
+edges <- graph$x$edges %>%
+  mutate(arrows = "to",
+         smooth = TRUE,
+         color = ifelse(to == 6, "red", "grey"),
+         width = value,
+         value = NULL)
+
+## Generate the custom network using `visNetwork`
+visNetwork(nodes, edges) %>%
+  visOptions(nodesIdSelection = list(enabled = TRUE)) 
