7bf69ccf4a096367a833b3e01d77fb43aed40db2
Colby Ford <colbytylerford@gmail.com>
2022-06-08 18:26:53 -0400

    Update network_generation.R

--- network_generation.R
+++ network_generation.R
@@ -2,7 +2,7 @@
 library(strainhub)
 
 
-### JANESSA
+### First Network
 ## Read in tree, metadata, and geodata
 treedata <- ape::read.tree("RAxML_bestTree.LMcat_karen_galvan_RC_v7_Dipsacaster_outDEDUP.Janesssa.tre")
 metadata <- readr::read_csv("LMcat_karen_galvan_RC_v7_Dipsacaster_outDEDUP.Janesssa16s.csv", col_names = TRUE)
@@ -15,11 +15,13 @@ graph <- makeTransNet(treedata,
                       centralityMetric = 6,
                       treeType = "parsimonious")
 
-print(graph)
+graph
+
+saveRDS(graph, "graph1.rds")
 
 
 
-### 
+### Second Network
 ## Read in tree, metadata, and geodata
 treedata <- ape::read.tree("RAxML_bestTree.COI_nataly_brenda_JANESSA.DEDUP.trim.tre")
 metadata <- readr::read_csv("RAxML_bestTree.COI_nataly_brenda_JANESSA.DEDUP.trim.out_101.fix.csv", col_names = TRUE)
@@ -32,3 +34,5 @@ graph <- makeTransNet(treedata,
                       centralityMetric = 6,
                       treeType = "parsimonious")
 graph
+
+saveRDS(graph, "graph2.rds")
