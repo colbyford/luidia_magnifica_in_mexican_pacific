0c1a8e21950cd6a7dcf61f1122fd967bfcdb0e15
Colby Ford <colbytylerford@gmail.com>
2022-06-08 16:57:42 -0400

    Create network_generation.R

---
+++ network_generation.R
@@ -0,0 +1,34 @@
+## Load in StrainHub
+library(strainhub)
+
+
+### JANESSA
+## Read in tree, metadata, and geodata
+treedata <- ape::read.tree("RAxML_bestTree.LMcat_karen_galvan_RC_v7_Dipsacaster_outDEDUP.Janesssa.tre")
+metadata <- readr::read_csv("LMcat_karen_galvan_RC_v7_Dipsacaster_outDEDUP.Janesssa16s.csv", col_names = TRUE)
+
+
+## Make the Transmission Network
+graph <- makeTransNet(treedata,
+                      metadata,
+                      columnSelection = "Body_of_Water",
+                      centralityMetric = 6,
+                      treeType = "parsimonious")
+
+print(graph)
+
+
+
+### 
+## Read in tree, metadata, and geodata
+treedata <- ape::read.tree("RAxML_bestTree.COI_nataly_brenda_JANESSA.DEDUP.trim.tre")
+metadata <- readr::read_csv("RAxML_bestTree.COI_nataly_brenda_JANESSA.DEDUP.trim.out_101.fix.csv", col_names = TRUE)
+
+
+## Make the Transmission Network
+graph <- makeTransNet(treedata,
+                      metadata,
+                      columnSelection = "Body_of_Water",
+                      centralityMetric = 6,
+                      treeType = "parsimonious")
+graph
