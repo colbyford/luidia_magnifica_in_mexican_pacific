f863363d563977f9eb6aa1f5c7ca56b0e08b8adf
Colby Ford <colbytylerford@gmail.com>
2022-06-26 10:19:56 -0400

    Update network_generation.R

--- network_generation.R
+++ network_generation.R
@@ -12,29 +12,29 @@ treedata <- ape::read.tree("Galvan_16S_54taxa/RAxML_bestTree.LMcat_karen_galvan_
 metadata <- readr::read_csv("Galvan_16S_54taxa/LMcat_karen_galvan_RC_v7_Dipsacaster_outDEDUP.Janesssa16s.csv", col_names = TRUE)
 
 selected_meta = "Body_of_Water_formatted"
-ancestralStates = asr_max_parsimony(treedata,
-                                    as.numeric(as.factor(metadata[[selected_meta]])),
-                                    length(unique(metadata[[selected_meta]])))
-
-
-asr <- ancestralStates$ancestral_likelihoods %>%
-  data.frame() %>%
-  mutate(max_col_first = max.col(., ties.method = "first"),
-         max_col_last =  max.col(., ties.method = "last"),
-         ambig = ifelse(max_col_first == max_col_last, FALSE, TRUE),
-         # taxa = treedata$tip.label[1:(length(treedata$tip.label)-1)]
-         taxa = rev(treedata$tip.label[1:(length(treedata$tip.label)-1)])
-         ) %>% 
-  select(!c(max_col_first, max_col_last))
-
-colnames(asr) <- c(sort(unique(metadata[[selected_meta]])), "ambig", "taxa")
+# ancestralStates = asr_max_parsimony(treedata,
+#                                     as.numeric(as.factor(metadata[[selected_meta]])),
+#                                     length(unique(metadata[[selected_meta]])))
+# 
+# 
+# asr <- ancestralStates$ancestral_likelihoods %>%
+#   data.frame() %>%
+#   mutate(max_col_first = max.col(., ties.method = "first"),
+#          max_col_last =  max.col(., ties.method = "last"),
+#          ambig = ifelse(max_col_first == max_col_last, FALSE, TRUE),
+#          # taxa = treedata$tip.label[1:(length(treedata$tip.label)-1)]
+#          taxa = rev(treedata$tip.label[1:(length(treedata$tip.label)-1)])
+#          ) %>% 
+#   select(!c(max_col_first, max_col_last))
+# 
+# colnames(asr) <- c(sort(unique(metadata[[selected_meta]])), "ambig", "taxa")
 
 
 # non_ambig_taxa <- asr %>% filter(!ambig) %>% select(taxa) %>% .[["taxa"]]
-ambig_taxa <- asr %>% filter(ambig) %>% select(taxa) %>% .[["taxa"]]
-
-
-non_ambig_treedata <- treedata %>% ape::drop.tip(ambig_taxa)
+# ambig_taxa <- asr %>% filter(ambig) %>% select(taxa) %>% .[["taxa"]]
+# 
+# 
+# non_ambig_treedata <- treedata %>% ape::drop.tip(ambig_taxa)
 
 
 ## Make the Transmission Network
@@ -45,11 +45,11 @@ full_graph <- makeTransNet(treedata,
                            treeType = "parsimonious")
 
 
-non_ambig_graph <- makeTransNet(non_ambig_treedata,
-                                metadata,
-                                columnSelection = selected_meta,
-                                centralityMetric = 3,
-                                treeType = "parsimonious")
+# non_ambig_graph <- makeTransNet(non_ambig_treedata,
+#                                 metadata,
+#                                 columnSelection = selected_meta,
+#                                 centralityMetric = 3,
+#                                 treeType = "parsimonious")
 
 # graph
 
@@ -61,10 +61,10 @@ non_ambig_graph <- makeTransNet(non_ambig_treedata,
 
 
 ## Determine Ambiguous Edges
-non_ambig_df <- non_ambig_graph$x$edges %>% 
-  inner_join(non_ambig_graph$x$nodes, by=c('from'='id'), suffix = c("", "_from")) %>% 
-  inner_join(non_ambig_graph$x$nodes, by=c('to'='id'),  suffix = c("", "_to")) %>% 
-  mutate(transmission = paste0(label,">", label_to))
+# non_ambig_df <- non_ambig_graph$x$edges %>% 
+#   inner_join(non_ambig_graph$x$nodes, by=c('from'='id'), suffix = c("", "_from")) %>% 
+#   inner_join(non_ambig_graph$x$nodes, by=c('to'='id'),  suffix = c("", "_to")) %>% 
+#   mutate(transmission = paste0(label,">", label_to))
 
 
 full_df <- full_graph$x$edges %>% 
@@ -79,7 +79,8 @@ full_df <- full_graph$x$edges %>%
   mutate(ambig = ifelse(label_to == "Club de Yates", TRUE, FALSE))
   
 
-colors_16s <- readr::read_csv("Galvan_16S_54taxa/Galvan_16S_colors.csv")
+# colors_16s <- readr::read_csv("Galvan_16S_54taxa/Galvan_16S_colors.csv")
+colors_greys <- c("#FFFFFF", grDevices::gray.colors(100))
 
 nodes <- full_graph$x$nodes %>%
   mutate(shape = "dot",
@@ -89,7 +90,13 @@ nodes <- full_graph$x$nodes %>%
                        "Bering Sea",
                        "Persian Gulf",
                        "North Pacific Ocean")) %>% 
-  inner_join(colors_16s, by = c("label" = "Body_of_Water_formatted"))
+  mutate(value_percentiles = as.integer(value/max(value)*100)+1)
+  # inner_join(colors_16s, by = c("label" = "Body_of_Water_formatted"))
+
+for (i in 1:nrow(nodes)){
+  nodes$color[i] <- colors_greys[nodes$value_percentiles[i]]
+}
+
   
 edges <- full_df %>% 
     mutate(arrows = "to",
@@ -127,25 +134,25 @@ visNetwork(nodes, edges) %>%
   visOptions(nodesIdSelection = list(enabled = TRUE))
 
 
-## Recolor Nodes and Edges
-color_palette <- distinctColorPalette(nrow(non_ambig_graph$x$nodes))
-
-nodes <- non_ambig_graph$x$nodes %>%
-  mutate(shape = "dot",
-         color = color_palette,
-         font.size = 20)
-
-edges <- non_ambig_graph$x$edges %>%
-  mutate(arrows = "to",
-         smooth = TRUE,
-         # dashes = full_df$ambig,
-         color = ifelse(to == 5, "red", "grey"),
-         width = value,
-         value = NULL)
-
-## Generate the custom network using `visNetwork`
-visNetwork(nodes, edges) %>%
-  visOptions(nodesIdSelection = list(enabled = TRUE))
+# ## Recolor Nodes and Edges
+# color_palette <- distinctColorPalette(nrow(non_ambig_graph$x$nodes))
+# 
+# nodes <- non_ambig_graph$x$nodes %>%
+#   mutate(shape = "dot",
+#          color = color_palette,
+#          font.size = 20)
+# 
+# edges <- non_ambig_graph$x$edges %>%
+#   mutate(arrows = "to",
+#          smooth = TRUE,
+#          # dashes = full_df$ambig,
+#          color = ifelse(to == 5, "red", "grey"),
+#          width = value,
+#          value = NULL)
+# 
+# ## Generate the custom network using `visNetwork`
+# visNetwork(nodes, edges) %>%
+#   visOptions(nodesIdSelection = list(enabled = TRUE))
 
 
 
@@ -159,29 +166,29 @@ metadata <- readr::read_csv("Galvan_COI_101taxa/RAxML_bestTree.COI_nataly_brenda
 
 
 selected_meta = "Body_of_Water_formatted"
-ancestralStates = asr_max_parsimony(treedata,
-                                    as.numeric(as.factor(metadata[[selected_meta]])),
-                                    length(unique(metadata[[selected_meta]])))
-
-
-asr <- ancestralStates$ancestral_likelihoods %>%
-  data.frame() %>%
-  mutate(max_col_first = max.col(., ties.method = "first"),
-         max_col_last =  max.col(., ties.method = "last"),
-         ambig = ifelse(max_col_first == max_col_last, FALSE, TRUE),
-         # taxa = treedata$tip.label[1:(length(treedata$tip.label)-1)]
-         taxa = rev(treedata$tip.label[1:(length(treedata$tip.label)-1)])
-         ) %>% 
-  select(!c(max_col_first, max_col_last))
-
-colnames(asr) <- c(sort(unique(metadata[[selected_meta]])), "ambig", "taxa")
+# ancestralStates = asr_max_parsimony(treedata,
+#                                     as.numeric(as.factor(metadata[[selected_meta]])),
+#                                     length(unique(metadata[[selected_meta]])))
+# 
+# 
+# asr <- ancestralStates$ancestral_likelihoods %>%
+#   data.frame() %>%
+#   mutate(max_col_first = max.col(., ties.method = "first"),
+#          max_col_last =  max.col(., ties.method = "last"),
+#          ambig = ifelse(max_col_first == max_col_last, FALSE, TRUE),
+#          # taxa = treedata$tip.label[1:(length(treedata$tip.label)-1)]
+#          taxa = rev(treedata$tip.label[1:(length(treedata$tip.label)-1)])
+#          ) %>% 
+#   select(!c(max_col_first, max_col_last))
+# 
+# colnames(asr) <- c(sort(unique(metadata[[selected_meta]])), "ambig", "taxa")
 
 
 # non_ambig_taxa <- asr %>% filter(!ambig) %>% select(taxa) %>% .[["taxa"]]
-ambig_taxa <- asr %>% filter(ambig) %>% select(taxa) %>% .[["taxa"]]
-
-
-non_ambig_treedata <- treedata %>% ape::drop.tip(ambig_taxa)
+# ambig_taxa <- asr %>% filter(ambig) %>% select(taxa) %>% .[["taxa"]]
+# 
+# 
+# non_ambig_treedata <- treedata %>% ape::drop.tip(ambig_taxa)
 
 
 ## Make the Transmission Network
@@ -192,11 +199,11 @@ full_graph <- makeTransNet(treedata,
                            treeType = "parsimonious")
 
 
-non_ambig_graph <- makeTransNet(non_ambig_treedata,
-                                metadata,
-                                columnSelection = selected_meta,
-                                centralityMetric = 3,
-                                treeType = "parsimonious")
+# non_ambig_graph <- makeTransNet(non_ambig_treedata,
+#                                 metadata,
+#                                 columnSelection = selected_meta,
+#                                 centralityMetric = 3,
+#                                 treeType = "parsimonious")
 
 # graph
 
@@ -205,10 +212,10 @@ non_ambig_graph <- makeTransNet(non_ambig_treedata,
 # full_graph <- readRDS("Galvan_COI_101taxa/graph.rds")
 
 ## Determine Ambiguous Edges
-non_ambig_df <- non_ambig_graph$x$edges %>% 
-  inner_join(non_ambig_graph$x$nodes, by=c('from'='id'), suffix = c("", "_from")) %>% 
-  inner_join(non_ambig_graph$x$nodes, by=c('to'='id'),  suffix = c("", "_to")) %>% 
-  mutate(transmission = paste0(label,">", label_to))
+# non_ambig_df <- non_ambig_graph$x$edges %>% 
+#   inner_join(non_ambig_graph$x$nodes, by=c('from'='id'), suffix = c("", "_from")) %>% 
+#   inner_join(non_ambig_graph$x$nodes, by=c('to'='id'),  suffix = c("", "_to")) %>% 
+#   mutate(transmission = paste0(label,">", label_to))
 
 
 full_df <- full_graph$x$edges %>% 
@@ -226,6 +233,7 @@ full_df <- full_graph$x$edges %>%
 # full_df$ambig <- ifelse(full_df$transmission %in% ambigs, TRUE, FALSE)
   
 colors_coi <- readr::read_csv("Galvan_COI_101taxa/Galvan_COI_colors.csv")
+colors_greys <- c("#FFFFFF", grDevices::gray.colors(100))
 
 nodes <- full_graph$x$nodes %>%
   mutate(shape = "dot",
@@ -233,7 +241,13 @@ nodes <- full_graph$x$nodes %>%
          font.size = 20) %>% 
   filter(!label %in% c("?",
                        "Queen Charlotte Islands")) %>% 
-  inner_join(colors_coi, by = c("label" = "Body_of_Water_formatted"))
+  mutate(value_percentiles = as.integer(value/max(value)*100)+1)
+# inner_join(colors_coi, by = c("label" = "Body_of_Water_formatted"))
+
+for (i in 1:nrow(nodes)){
+  nodes$color[i] <- colors_greys[nodes$value_percentiles[i]]
+}
+
 
 edges <- full_df %>% 
   mutate(arrows = "to",
@@ -266,21 +280,21 @@ visNetwork(nodes, edges) %>%
 
 
 ## Recolor Nodes and Edges
-color_palette <- distinctColorPalette(nrow(non_ambig_graph$x$nodes))
-
-nodes <- non_ambig_graph$x$nodes %>%
-  mutate(shape = "dot",
-         color = color_palette,
-         font.size = 20)
-
-edges <- non_ambig_graph$x$edges %>%
-  mutate(arrows = "to",
-         smooth = TRUE,
-         # dashes = full_df$ambig,
-         color = ifelse(to == 6, "red", "grey"),
-         width = value,
-         value = NULL)
-
-## Generate the custom network using `visNetwork`
-visNetwork(nodes, edges) %>%
-  visOptions(nodesIdSelection = list(enabled = TRUE))
+# color_palette <- distinctColorPalette(nrow(non_ambig_graph$x$nodes))
+# 
+# nodes <- non_ambig_graph$x$nodes %>%
+#   mutate(shape = "dot",
+#          color = color_palette,
+#          font.size = 20)
+# 
+# edges <- non_ambig_graph$x$edges %>%
+#   mutate(arrows = "to",
+#          smooth = TRUE,
+#          # dashes = full_df$ambig,
+#          color = ifelse(to == 6, "red", "grey"),
+#          width = value,
+#          value = NULL)
+# 
+# ## Generate the custom network using `visNetwork`
+# visNetwork(nodes, edges) %>%
+#   visOptions(nodesIdSelection = list(enabled = TRUE))
