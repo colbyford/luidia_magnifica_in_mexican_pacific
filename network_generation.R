8afc3b9499dd8c9b7a44fdafa9879b2a7a97fd7f
Colby Ford <colbytylerford@gmail.com>
2023-05-15 13:50:16 -0400

    Update network_generation.R

--- network_generation.R
+++ network_generation.R
@@ -142,7 +142,7 @@ write_csv(output_graph$x$nodes, "betweenness_fix_grey_COI/COI_network_strainhub_
 treedata <- ape::read.tree("2023May_run/w-baRnfBpf1L0Yw4jdGNxQ_newick.txt")
 metadata <- readr::read_csv("2023May_run/SD_TaiwanCOIv3_DJ_body_of_waterFIX2jpl2.csv", col_names = TRUE)
 
-selected_meta = "acro"
+selected_meta = "Body_of_Water"
 
 ## Make the Transmission Network
 full_graph <- makeTransNet(treedata,
@@ -155,13 +155,40 @@ full_graph <- makeTransNet(treedata,
 full_df <- full_graph$x$edges %>% 
   inner_join(full_graph$x$nodes, by=c('from'='id'), suffix = c("", "_from")) %>% 
   inner_join(full_graph$x$nodes, by=c('to'='id'),  suffix = c("", "_to")) %>% 
-  mutate(transmission = paste0(label,">", label_to)) %>% 
+  mutate(label = case_when(
+    label == "a" ~ "EIWP",
+    label == "b" ~ "SANZ",
+    label == "c" ~ "NWP",
+    label == "d" ~ "WIO",
+    label == "e" ~ "NEP",
+    label == "f" ~ "NWA",
+    label == "g" ~ "EP",
+    label == "h" ~ "WA",
+    label == "i" ~ "NEA",
+    label == "k" ~ "SAFR",
+    .default = as.character(label)
+  ),
+  label_to = case_when(
+    label_to == "a" ~ "EIWP",
+    label_to == "b" ~ "SANZ",
+    label_to == "c" ~ "NWP",
+    label_to == "d" ~ "WIO",
+    label_to == "e" ~ "NEP",
+    label_to == "f" ~ "NWA",
+    label_to == "g" ~ "EP",
+    label_to == "h" ~ "WA",
+    label_to == "i" ~ "NEA",
+    label_to == "k" ~ "SAFR",
+    .default = as.character(label_to)
+  ),
+  ) %>% 
+  mutate(transmission = paste0(label,">", label_to))# %>% 
   ## Manual Edge Selection/Ambiguity
-  filter(!transmission %in% c("Almirante Bay>Bering Sea",
-                              "Almirante Bay>North Pacific Ocean",
-                              "Almirante Bay>Persian Gulf",
-                              "Gulf of Thailand>?")) %>% 
-  mutate(ambig = ifelse(label_to == "Club de Yates", TRUE, FALSE))
+  # filter(!transmission %in% c("Almirante Bay>Bering Sea",
+  #                             "Almirante Bay>North Pacific Ocean",
+  #                             "Almirante Bay>Persian Gulf",
+  #                             "Gulf of Thailand>?")) %>% 
+  # mutate(ambig = ifelse(label_to == "Club de Yates", TRUE, FALSE))
 
 
 # colors_16s <- readr::read_csv("Galvan_16S_54taxa/Galvan_16S_colors.csv")
@@ -170,10 +197,23 @@ colors_greys <- c("#FFFFFF", rev(grDevices::gray.colors(100)))
 nodes <- full_graph$x$nodes %>%
   mutate(shape = "dot",
          font.size = 20) %>% 
-  filter(!label %in% c("?",
-                       "Bering Sea",
-                       "Persian Gulf",
-                       "North Pacific Ocean")) %>% 
+  mutate(label = case_when(
+    label == "a" ~ "EIWP",
+    label == "b" ~ "SANZ",
+    label == "c" ~ "NWP",
+    label == "d" ~ "WIO",
+    label == "e" ~ "NEP",
+    label == "f" ~ "NWA",
+    label == "g" ~ "EP",
+    label == "h" ~ "WA",
+    label == "i" ~ "NEA",
+    label == "k" ~ "SAFR",
+    .default = as.character(label)
+  )) %>% 
+  # filter(!label %in% c("?",
+  #                      "Bering Sea",
+  #                      "Persian Gulf",
+  #                      "North Pacific Ocean")) %>% 
   mutate(value_percentiles = as.integer(value/max(value)*100)+1)
 # inner_join(colors_16s, by = c("label" = "Body_of_Water_formatted"))
 
@@ -185,10 +225,17 @@ for (i in 1:nrow(nodes)){
 edges <- full_df %>% 
   mutate(arrows = "to",
          smooth = TRUE,
-         dashes = full_df$ambig,
-         color = ifelse(to == 5, "red", "grey"),
+         # dashes = full_df$ambig,
+         color = "grey",
+         # color = ifelse(to == 5, "red", "grey"),
          width = value) %>% 
-  select(from, to, arrows, smooth, dashes, color, width)
+  select(from,
+         to,
+         arrows,
+         smooth,
+         # dashes,
+         color,
+         width)
 
 
 ## Generate the custom network using `visNetwork`
