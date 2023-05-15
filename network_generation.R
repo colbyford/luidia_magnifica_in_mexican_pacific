c6bf77bce9ca1109fb8163b53f806698fecd1585
Colby Ford <colbytylerford@gmail.com>
2023-05-14 21:13:03 -0400

    Update network_generation.R

--- network_generation.R
+++ network_generation.R
@@ -134,3 +134,67 @@ output_graph
 
 write_csv(output_graph$x$nodes, "betweenness_fix_grey_COI/COI_network_strainhub_output.csv")
 
+
+
+############################
+### May 2023 Data - 25 Taxa
+## Read in tree, metadata, and geodata
+treedata <- ape::read.tree("2023May_run/w-baRnfBpf1L0Yw4jdGNxQ_newick.txt")
+metadata <- readr::read_csv("2023May_run/SD_TaiwanCOIv3_DJ_body_of_waterFIX2jpl2.csv", col_names = TRUE)
+
+selected_meta = "acro"
+
+## Make the Transmission Network
+full_graph <- makeTransNet(treedata,
+                           metadata,
+                           columnSelection = selected_meta,
+                           centralityMetric = 3,
+                           treeType = "parsimonious")
+
+## Convert Graph info to DataFrame
+full_df <- full_graph$x$edges %>% 
+  inner_join(full_graph$x$nodes, by=c('from'='id'), suffix = c("", "_from")) %>% 
+  inner_join(full_graph$x$nodes, by=c('to'='id'),  suffix = c("", "_to")) %>% 
+  mutate(transmission = paste0(label,">", label_to)) %>% 
+  ## Manual Edge Selection/Ambiguity
+  filter(!transmission %in% c("Almirante Bay>Bering Sea",
+                              "Almirante Bay>North Pacific Ocean",
+                              "Almirante Bay>Persian Gulf",
+                              "Gulf of Thailand>?")) %>% 
+  mutate(ambig = ifelse(label_to == "Club de Yates", TRUE, FALSE))
+
+
+# colors_16s <- readr::read_csv("Galvan_16S_54taxa/Galvan_16S_colors.csv")
+colors_greys <- c("#FFFFFF", rev(grDevices::gray.colors(100)))
+
+nodes <- full_graph$x$nodes %>%
+  mutate(shape = "dot",
+         font.size = 20) %>% 
+  filter(!label %in% c("?",
+                       "Bering Sea",
+                       "Persian Gulf",
+                       "North Pacific Ocean")) %>% 
+  mutate(value_percentiles = as.integer(value/max(value)*100)+1)
+# inner_join(colors_16s, by = c("label" = "Body_of_Water_formatted"))
+
+for (i in 1:nrow(nodes)){
+  nodes$color[i] <- colors_greys[nodes$value_percentiles[i]]
+}
+
+
+edges <- full_df %>% 
+  mutate(arrows = "to",
+         smooth = TRUE,
+         dashes = full_df$ambig,
+         color = ifelse(to == 5, "red", "grey"),
+         width = value) %>% 
+  select(from, to, arrows, smooth, dashes, color, width)
+
+
+## Generate the custom network using `visNetwork`
+output_graph <- visNetwork(nodes, edges) %>%
+  visOptions(nodesIdSelection = list(enabled = TRUE))
+
+output_graph
+
+write_csv(output_graph$x$nodes, "2023May_run/SD_TaiwanCOI_network_strainhub_output.csv")
